f348166d0f227b11c8718da50e3e7a3750fb2b0d
alsdud0301 <sms0751@naver.com>
2024-09-10 22:02:22 +0900

    세션 username 출력 테스트

--- client/src/components/make_test.tsx
+++ client/src/components/make_test.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Make_test = () =>{
+    const [username, setUsername] = useState<string | null>(null);
+    useEffect(()=> {
+        axios.get('/api/get-username')
+        .then((response)=>{
+            setUsername(response.data.username);
+        })
+    }, []);
     return(
-        <div className="test_div">
-            테스트 만드는 페이지입니다
-        </div>
+        <section className="test_section">
+            <div className="test_div">
+                세션에 저장된 이름 : {username}
+            </div>
+        </section>
     )
 } 
 
